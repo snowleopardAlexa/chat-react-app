28ca055e3f41d977f361a0dff6f42902bcbcf353
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-17 13:42:34 -0500

    add chat feed ui

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -33,11 +33,24 @@ const ChatFeed = (props) => {
         })
     }
 
+
+    if(!chat) return 'Loading...';
+
     return (
-        <div>
-            Chatfeed
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">{chat?.title}</div>
+                <div className="chat-subtitle">
+                    {chat.people.map((person) => `${person.person.username}`)}
+                </div>
+            </div>
+            {renderMessages()}
+        <div style={{ height: '100px'}} />
+        <div className="message-form-container">
+           <MessageForm {...props} chatId={activeChat} />
         </div>
-    )
-}
+        </div>
+    );
+};
 
 export default ChatFeed;
