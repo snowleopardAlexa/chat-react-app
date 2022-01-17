29b73cd0b755d303d2bd8a348d9a6371deee0d91
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-17 14:02:48 -0500

    style their messages look

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -12,6 +12,21 @@ const TheirMessage = ({ lastMessage, message }) => {
                  style={{backgroundImage: `url(${message?.sender?.avatar})`}}
                />
            )}
+           
+                {message?.attachements?.length > 0 
+                    ? (
+                        <img 
+                           src={message.attachements[0].file}
+                           alt="message-attachment"
+                           className="message-image"
+                           style={{marginLeft: isFirstMessageByUser ? '4px' : '48px'}}
+                        />
+                    ) : (
+                        <div className="message" style={{ float: 'left', backgroundColor: '#016064'}}>
+                          {message.text}
+                        </div> 
+                    )
+                }
         </div>
     )
 }
