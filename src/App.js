08370e4323e834c576313676c23dc7d1b07e75c7
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-17 12:56:26 -0500

    add chat engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <ChatEngine 
+        height="100vh"
+        projectID="2d338583-f0c7-4459-bf9f-c2e194b7e410"
+        userName="alexa"
+        userSecret="Valencia19"
+      />
     </div>
   );
 }
