0cca5ec3e39fb8da10d752c5acf440ca24b17ac6
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-17 14:26:36 -0500

    add form

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
 const MessageForm = (props) => {
 
@@ -11,13 +12,19 @@ const MessageForm = (props) => {
 
          const text = value.trim()
 
-         if(text.length > 0) sendMessage(creds, chatId, { text })
+         if(text.length > 0) sendMessage(creds, chatId, { text });
+
+         setValue('');
     }
 
     const handleChange = (event) => {
        setValue(event.target.value);
 
        isTyping(props, chatId);
+    }
+
+    const handleUpload = (event) => {
+        sendMessage(creds, chatId, {files: event.target.value, text: '' })
     }
 
     return (
@@ -29,6 +36,21 @@ const MessageForm = (props) => {
             onChange={handleChange}
             onSubmit={handleSubmit}
            />
+           <label htmlFor="upload-button">
+              <span className="image-button">
+                <PictureOutlined className="picture-icon" />
+              </span>
+           </label>
+           <input 
+             type="file"
+             multiple={false}
+             id="uplod-button"
+             style={{ display: 'none'}}
+             onChange={handleUpload}
+           />
+           <button type="submit" className="send-button">
+               <SendOutlined className="send-icon" />
+           </button>
         </form>
     )
 }
