0f5ffd2b22290f5a85fd47f9d88bb69d08f57b59
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-17 13:35:59 -0500

    add chat feed comp

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -8,7 +8,30 @@ const ChatFeed = (props) => {
 
     const chat = chats && chats[activeChat];
 
-    console.log(chat, userName, messages)
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index - 1];
+            const isMyMessage = userName === message.sender.username;
+
+            return (
+                <div key={`msg_${index}`} style={{ width: '100%'}}>
+                   <div className="message-block">
+                       {
+                           isMyMessage
+                           ? <MyMessage />
+                           : <TheirMessage />
+                       }
+                   </div>
+                   <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '18px' : '68px'}}>
+                       read receipts
+                   </div>
+                </div>
+            );
+        })
+    }
 
     return (
         <div>
